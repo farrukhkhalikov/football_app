aa8046ffbdd2f429278732661650710729d37569
farrukhkhalikov <farrukhkhalikov@yahoo.com>
2018-04-16 12:49:26 +0530

    adding react component for team

--- app/javascript/components/Team.js
+++ app/javascript/components/Team.js
@@ -3,21 +3,28 @@ import PropTypes from "prop-types"
 class Team extends React.Component {
   render () {
     const imgStyle = {height: "50px",width: "50px"}
-    const row = this.props.teams.map((data) =>
-      <tr>
-        <td key={data.name}>{data.name}</td>
-        <td key={data.shortName}>{data.shortName}</td>
-        <td>
-          <a href={"/stadiums?stadium_id="+ stadium.id}>{stadium.name}</a>
-        </td>
-        <td>
-          <img src={data.crestUrl} style={imgStyle} />
-        </td>
-        <td>
-          <a href={"/players?team_id="+ data.id}>Show Players</a>
-        </td>
-      </tr>
-      );
+    const row = []
+    const stadiums = this.props.stadiums
+
+    for (var i = 0; i < this.props.teams.length; i++) {
+      var data = this.props.teams[i]
+      var stadium = stadiums[Math.floor(Math.random()*stadiums.length)];
+      row.push(
+        <tr>
+          <td key={data.name}>{data.name}</td>
+          <td key={data.shortName}>{data.shortName}</td>
+          <td>
+            <a href={"/stadiums?stadium_id="+ stadium.id}>{stadium.name}</a>
+          </td>
+          <td>
+            <img src={data.crestUrl} style={imgStyle} />
+          </td>
+          <td>
+            <a href={"/players?team_id="+ data.id}>Show Players</a>
+          </td>
+        </tr>
+      )
+    }
       return (
       <table class="table">
         <thead>
