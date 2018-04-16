2d94d99a834ed044d6eb82de55f29f4d65ccd151
farrukhkhalikov <farrukhkhalikov@yahoo.com>
2018-04-16 13:14:34 +0530

    modification on player components

--- app/javascript/components/Player.js
+++ app/javascript/components/Player.js
@@ -4,25 +4,27 @@ class Player extends React.Component {
   render () {
     const row = this.props.players.map((data) =>
       <tr>
-        <td key={data.name}>{data.name}</td>
-        <td key={data.position}>{data.position}</td>
-        <td key={data.jerseyNumber}>{data.jerseyNumber}</td>
-        <td key={data.dateOfBirth}>{data.dateOfBirth}</td>
-        <td key={data.nationality}>{data.nationality}</td>
-        <td key={data.contractUntil}>{data.contractUntil}</td>
-        <td key={data.marketValue}>{data.marketValue}</td>
+        <td>{data.name}</td>
+        <td>{data.position}</td>
+        <td>{data.jerseyNumber}</td>
+        <td>{data.dateOfBirth}</td>
+        <td>{data.nationality}</td>
+        <td>{data.contractUntil}</td>
+        <td>{data.marketValue}</td>
       </tr>
       );
       return (
-        <table class="table">
+        <table className="table">
           <thead>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Jersey Number</th>
-            <th>Date Of Birth</th>
-            <th>Nationality</th>
-            <th>Contract Until</th>
-            <th>Market Value</th>
+            <tr>
+              <th>Name</th>
+              <th>Position</th>
+              <th>Jersey Number</th>
+              <th>Date Of Birth</th>
+              <th>Nationality</th>
+              <th>Contract Until</th>
+              <th>Market Value</th>
+            </tr>
           </thead>
           <tbody>
             {row}
